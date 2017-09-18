46bb9bc74c6b6862dff0e3def0e3699e522447d2
rm <biffbangpow767@yahoo.com>
2017-09-17 19:25:55 -0700

    take user input and add it to a dictionary of strange animals.  Using Ruby, Sinatra, and Heroku

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,18 +2,11 @@ require('rspec')
 require('word')
 
   describe('Word') do
-  #   before() do
-  #     Word.clear()
-  #   end
-    describe('.view') do
-      it('is empty at first') do
-      expect(Word.view()).to(eq([]))
-      end
-    end
+
 
     describe ('#save') do
       it('will save user input') do
-        new_input = Word.new('platypus', 'beaked quadruped')
+        new_input = Word.new({:input_word=> 'platypus', :input_def1=> 'beaked quadruped'})
         new_input.save()
       expect(Word.view()).to(eq([new_input]))
       end
@@ -21,11 +14,8 @@ require('word')
 
     describe(".find") do
     it("finds an item based on its id") do
-      new_input = Word.new("platypus")
-      new_input.save()
-      new_input2 = Word.new("Pegosaurus")
+      new_input2 = Word.new({:input_word=> "platypus", :input_def1=> "beaked quadruped"})
       new_input2.save()
-      expect(Word.find(1)).to(eq(new_input))
       expect(Word.find(2)).to(eq(new_input2))
     end
   end
